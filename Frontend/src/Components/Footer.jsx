1a5e47b0005a675ebe79542427f7c97f45e3193a
prabhu5211 <prabhu5211>
2025-03-31 20:37:02 +0530

    sai

--- Frontend/src/Components/Footer.jsx
+++ Frontend/src/Components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
     return (
       <footer className="bg-gray-800 text-white p-4">
         <div className="container mx-auto text-center">
-          <p>&copy; 2025 Global Visa Corp. All rights reserved.</p>
+          <p>&copy; Victory Visas. All rights reserved.</p>
         </div>
       </footer>
     );
